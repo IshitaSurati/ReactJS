25808c09b5bb22c9586ffadd3e294b95fb67a17a
Ishitasurat25 <ishitasurati83@gmail.com>
2025-02-06 15:43:06 +0530

    backend setup

---
+++ Project/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv').config();
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    console.log("Welcome to bakend of project");
+
+})
+
+
+
+
+const PORT = process.env.PORT || 8090
+app.listen(8090, () => {
+    console.log(`Server Runing on port ${PORT}`);
+})
+connectDB();
